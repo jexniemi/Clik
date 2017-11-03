cd1ebc60c7a4639276d942590d2ac15a6a902a8b
jexniemi <jxnieminen@gmail.com>
2017-11-04 01:15:48 +0200

    improved overall site appearance, added background image

--- src/App.js
+++ src/App.js
@@ -16,16 +16,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="Navbar">
-          <ul className="Nav-ul">
-            <li className="Nav-li"><h1 className="Site-Title">Clik!</h1></li>
-            <li className="Nav-button"><button className="button3">World</button></li>
-          </ul>
-        </div>
-        <div className="Lists">
-          <ListNews title="Helsingin Sanomat" url="https://www.hs.fi/rss/tuoreimmat.xml" />
-          <ListNews title="Ilta-Sanomat" url="https://www.is.fi/rss/tuoreimmat.xml" />
-          <ListNews title="Iltalehti" url="http://www.iltalehti.fi/rss/uutiset.xml" />
+                  <div className="Navbar">
+            <ul className="Nav-ul">
+              <li className="Nav-li"><h1 className="Site-Title">Clik!</h1></li>
+              <li className="Nav-button"><button className="button3">World</button></li>
+            </ul>
+          </div>
+        <div className="Content">
+          <div className="Lists">
+            <ListNews title="Helsingin Sanomat" url="https://www.hs.fi/rss/tuoreimmat.xml" />
+            <ListNews title="Ilta-Sanomat" url="https://www.is.fi/rss/tuoreimmat.xml" />
+            <ListNews title="Iltalehti" url="http://www.iltalehti.fi/rss/uutiset.xml" />
+          </div>
         </div>
       </div>
     );
