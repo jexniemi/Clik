9e644a201d739ab2f346ba5ad003d2f7c2520f08
jexniemi <jxnieminen@gmail.com>
2017-11-04 11:22:04 +0200

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './css/App.css';
 import ListNews from './components/ListNews';
 /*eslint-env jquery*/
