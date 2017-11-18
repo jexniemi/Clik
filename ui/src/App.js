39108411de6decd8ed0a86306a16c643098051ac
jexniemi <jxnieminen@gmail.com>
2017-11-18 17:29:09 +0200

    mtv changed to mtv.fi

--- ui/src/App.js
+++ ui/src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
           <div className="Lists">
             <ListNews title="Helsingin Sanomat" url="https://www.hs.fi/rss/tuoreimmat.xml" />
             <ListNews title="Ilta-Sanomat" url="https://www.is.fi/rss/tuoreimmat.xml" />
-            <ListNews title="MTV" url="https://www.mtv.fi/api/feed/rss/uutiset_uusimmat" />
+            <ListNews title="MTV.fi" url="https://www.mtv.fi/api/feed/rss/uutiset_uusimmat" />
           </div>
         </div>
       </div>
